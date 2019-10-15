5f48d99c96e41f8b1f030503d0b6a2dbbbabe5f2
Iliyan Markov <iliyan@iliyan.com>
2019-10-15 18:16:40 +0300

    list view for beers

--- moonshine/beer/urls.py
+++ moonshine/beer/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from beer.views import BeerView
+from beer.views import BeerView, BeerListView
 
 urlpatterns = [
     url(r'^beer/$', BeerView.as_view(), name='beer-post'),
     url(r'^beer/(?P<pk>.*)/$', BeerView.as_view(), name='beer-get'),
+    url(r'^list/beer/$', BeerListView.as_view(), name='beer-list'),
 ]
 
 beer_api_urls = urlpatterns
