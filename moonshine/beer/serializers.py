887c95619ac4012889fc95616a7cc1dfc0277280
Iliyan Markov <iliyan@iliyan.com>
2019-10-15 18:16:28 +0300

    add serializer fields

--- moonshine/beer/serializers.py
+++ moonshine/beer/serializers.py
@@ -4,6 +4,11 @@ from beer.models import BeerModel
 
 class BeerModelSerializer(serializers.ModelSerializer):
 
+    name = serializers.CharField(max_length=50)
+    beer_type = serializers.ChoiceField(
+         choices=(('AL', 'Ale'), ('LA', 'Lager'), ('ST', 'Stout')))
+    description = serializers.CharField(max_length=500)
+
     class Meta:
         model = BeerModel
 
