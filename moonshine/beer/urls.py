f783006020874fccfd0c38e23c67a9f8484fe448
IvanStefanov <ivanxstefanov@gmail.com>
2020-07-22 02:24:12 +0300

    tweaked list endpoint

--- moonshine/beer/urls.py
+++ moonshine/beer/urls.py
@@ -4,7 +4,7 @@ from beer.views import BeerView, BeerListView
 urlpatterns = [
     url(r'^beer/$', BeerView.as_view(), name='beer-post'),
     url(r'^beer/(?P<pk>.*)/$', BeerView.as_view(), name='beer-get'),
-    url(r'^list/beer/$', BeerListView.as_view(), name='beer-list'),
+    url(r'^beers/$', BeerListView.as_view(), name='beer-list'),
 ]
 
 beer_api_urls = urlpatterns
